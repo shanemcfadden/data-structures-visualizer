46a6dae2b221659d3abf719cc687edaab3180232
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-10-25 17:42:10 -0500

    Add data-structures-visualizer base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/data-structures-visualizer",
   plugins: [react(), tailwindcss()],
 });
