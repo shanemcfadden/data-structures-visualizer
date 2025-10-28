ccf6de6b7abfcb6e2666865939865620efd8b002
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-10-27 19:53:11 -0500

    Lint

--- src/views/LinkedListView/LinkedListDashboard/index.tsx
+++ src/views/LinkedListView/LinkedListDashboard/index.tsx
@@ -2,17 +2,15 @@ import { LinkedListProperties } from "./LinkedListProperties";
 import { LinkedListMethods } from "./LinkedListMethods";
 import { Margin } from "../../../components/Margin";
 
-export const LinkedListDashboard = () => {
-  return (
-    <Margin>
-      <div className="sm:hidden">
-        <LinkedListProperties />
-        <LinkedListMethods />
-      </div>
-      <div className="hidden sm:grid sm:grid-cols-2">
-        <LinkedListProperties collapseTopMargin />
-        <LinkedListMethods collapseTopMargin />
-      </div>
-    </Margin>
-  );
-};
+export const LinkedListDashboard = () => (
+  <Margin>
+    <div className="sm:hidden">
+      <LinkedListProperties />
+      <LinkedListMethods />
+    </div>
+    <div className="hidden sm:grid sm:grid-cols-2">
+      <LinkedListProperties collapseTopMargin />
+      <LinkedListMethods collapseTopMargin />
+    </div>
+  </Margin>
+);
