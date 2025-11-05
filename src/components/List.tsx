a205876a238c8e2e0cec7134c55bf2cab9028881
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-11-04 19:50:07 -0600

    Lint

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,5 @@
 import type { PropsWithChildren } from "react";
 
-export const UnorderedList = ({ children }: PropsWithChildren) => {
-  return <ul className="list-disc list-inside">{children}</ul>;
-};
+export const UnorderedList = ({ children }: PropsWithChildren) => <ul className="list-disc list-inside">{children}</ul>;
 
-export const ListItem = ({ children }: PropsWithChildren) => {
-  return <li>{children}</li>;
-};
+export const ListItem = ({ children }: PropsWithChildren) => <li>{children}</li>;
