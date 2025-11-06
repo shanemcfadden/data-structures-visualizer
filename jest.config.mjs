27d0dafb247c9599432613770e4b239c5221440b
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-11-06 05:40:43 -0600

    Lint and Format

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,16 +1,15 @@
-import type { Config } from "jest";
-const nextJest = require("next/jest.js").default;
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   dir: "./",
 });
 
 // Add any custom config to be passed to Jest
-const config: Config = {
+const config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
-module.exports = createJestConfig(config);
+export default createJestConfig(config);
