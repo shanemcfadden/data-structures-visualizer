753f2fe94fb89ad10eb0d12e015966148cc71ab4
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-11-19 21:22:11 -0600

    Init linked list property tests

--- cypress/e2e/linked-list.cy.ts
+++ cypress/e2e/linked-list.cy.ts
@@ -1,6 +1,22 @@
 describe("linked list", () => {
   it("passes", () => {
     cy.visit("/");
+
+    cy.get("main").within(() => {
+      cy.contains("Linked List").click();
+    });
+
+    cy.url().should("include", "/linked-list");
+
+    cy.get('[data-cy="linked-list-property-head"]')
+      .should("contain", "Head")
+      .should("contain", "null");
+    cy.get('[data-cy="linked-list-property-tail"]')
+      .should("contain", "Tail")
+      .should("contain", "null");
+    cy.get('[data-cy="linked-list-property-length"]')
+      .should("contain", "Length")
+      .should("contain", "0");
   });
 });
 
