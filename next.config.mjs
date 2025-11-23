a344f6c34b2fb8ccddee4ffee3eca72a2d77c6ae
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-11-23 09:58:38 -0600

    Fix distDir

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
   output: "export",
-  distDir: `./dist${process.env.NEXT_PUBLIC_BASE_PATH ? `/${process.env.NEXT_PUBLIC_BASE_PATH}` : ""}`,
+  distDir: `./dist${process.env.NEXT_PUBLIC_BASE_PATH}`,
 };
 
 export default nextConfig;
