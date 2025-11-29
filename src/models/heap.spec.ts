94cc55c8424f4574cbeead4c2777133e6a82197c
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-11-29 08:30:30 -0600

    "Tidy

--- src/models/heap.spec.ts
+++ src/models/heap.spec.ts
@@ -100,8 +100,10 @@ describe("heap", () => {
       fc.assert(
         fc.property(fc.integer({ min: 1, max: 8 }), (heapRows) => {
           const heap = new MinHeap();
+
           for (let i = 0; i < heapRows; i++) {
             const numberOfMembersInRow = 2 ** i;
+
             for (let j = 0; j < numberOfMembersInRow; j++) {
               heap.insert(j);
             }
